f7a8687ad23c55ade699ad75e1e5f6d98de8f8d5
nenadbarjak <nenadbarjak@gmail.com>
2021-05-28 17:41:43 +0200

    Setup basic server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.listen(port, () => {
+    console.log(`Server is up on port ${port}`)
+})
